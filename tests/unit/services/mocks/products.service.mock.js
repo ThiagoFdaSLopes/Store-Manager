2dfa2851c986cd26b26dcfb65f3624760bd32f82
Thiago <programadorthiagolopes@gmail.com>
2023-01-13 17:36:31 -0300

    feat(mockService): mockado valores para service

---
+++ tests/unit/services/mocks/products.service.mock.js
@@ -0,0 +1,32 @@
+const INVALID_NUMBER = 'id';
+
+const TYPE_MESSAGE = "INVALID_VALUE";
+
+const MESSAGE_ERROR = '"id" must be a number';
+
+const PRODUCT_ID_ERROR = 'Product id error';
+const PRODUCT_NOT_FOUND = 'Product not found';
+
+const allProducts = [
+  {
+    "id": 1,
+    "name": "Martelo de Thor"
+  },
+  {
+    "id": 2,
+    "name": "Traje de encolhimento"
+  },
+  {
+    "id": 3,
+    "name": "Escudo do Capitão América"
+  }
+];
+
+module.exports = {
+  INVALID_NUMBER,
+  MESSAGE_ERROR,
+  TYPE_MESSAGE,
+  PRODUCT_ID_ERROR,
+  PRODUCT_NOT_FOUND,
+  allProducts,
+}
