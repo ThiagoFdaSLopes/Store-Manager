035e88364b58e23593cf07e8c13dd9dd4677f523
Thiago <programadorthiagolopes@gmail.com>
2023-01-13 20:13:21 -0300

    feat(mock): Criado mock para create

--- tests/unit/services/mocks/products.service.mock.js
+++ tests/unit/services/mocks/products.service.mock.js
@@ -1,11 +1,10 @@
 const INVALID_NUMBER = 'id';
-
 const TYPE_MESSAGE = "INVALID_VALUE";
-
 const MESSAGE_ERROR = '"id" must be a number';
-
 const PRODUCT_ID_ERROR = 'Product id error';
 const PRODUCT_NOT_FOUND = 'Product not found';
+const PRODUCT_NAME_ERROR = '"name" length must be at least 5 characters long';
+const PRODUCT_TYPE_NAME_ERROR = 'INVALID_NAME';
 
 const allProducts = [
   {
@@ -22,11 +21,19 @@ const allProducts = [
   }
 ];
 
+newProduct = {
+  id: 30,
+  name: "ProdutoX"
+};
+
 module.exports = {
   INVALID_NUMBER,
   MESSAGE_ERROR,
   TYPE_MESSAGE,
   PRODUCT_ID_ERROR,
   PRODUCT_NOT_FOUND,
+  PRODUCT_NAME_ERROR,
+  PRODUCT_TYPE_NAME_ERROR,
   allProducts,
-}
+  newProduct,
+};
