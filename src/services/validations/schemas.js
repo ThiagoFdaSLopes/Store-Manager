b8cbf44865393972484f22da741abc7911f5b75d
Thiago <programadorthiagolopes@gmail.com>
2023-01-13 18:46:20 -0300

    feat(schemas): Criado schemas para validar name

--- src/services/validations/schemas.js
+++ src/services/validations/schemas.js
@@ -1,7 +1,10 @@
 const Joi = require('joi');
 
-const idProduct = Joi.number().integer().min(1).required(); 
+const idProduct = Joi.number().integer().min(1).required();
+
+const productName = Joi.string().min(5).required();
 
 module.exports = {
   idProduct,
+  productName,
 };
