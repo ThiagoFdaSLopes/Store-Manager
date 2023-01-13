c8996f4b318ea1a325c5745a490e93488ce29f8c
Thiago <programadorthiagolopes@gmail.com>
2023-01-13 18:47:53 -0300

    feat: Nova rota para criar produto

--- src/routers/products.routers.js
+++ src/routers/products.routers.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const productsController = require('../controllers/products.controller');
+const { validateName } = require('../middlewares');
 
 const router = express.Router();
+
+router.post('/', validateName, productsController.createProduct);
 
 router.get('/', productsController.findAll);
 
