e5942fbade7ff7ed132e217aac3df7f577343eec
Thiago <programadorthiagolopes@gmail.com>
2023-01-13 16:35:46 -0300

    feat(service): Corrigido Service do Products

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -10,9 +10,9 @@ const findById = async (idProduct) => {
   const error = validateId(idProduct);
   if (error.type) return error;
 
-  const products = await productsModel.findById(idProduct);
-  if (products) return { type: null, message: products };
-  return { type: '', message: 'Product not found' };
+  const [product] = await productsModel.findById(idProduct);
+  if (product) return { type: null, message: product };
+  return { type: 'Product id error', message: 'Product not found' };
 };
 
 module.exports = {
