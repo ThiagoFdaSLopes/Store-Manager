bf0fe449196911a8959051c61421114059a79cae
Thiago <programadorthiagolopes@gmail.com>
2023-01-13 18:11:05 -0300

    feat(controller): Criado teste controller product

---
+++ tests/unit/controllers/productsController.test.js
@@ -0,0 +1,68 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const sinonChai = require('sinon-chai');
+
+const { expect } = chai;
+chai.use(sinonChai);
+
+const { productsService } = require('../../../src/services');
+const { productsController } = require('../../../src/controllers')
+
+
+const { productsList } = require('./mock/products.controller.mock');
+
+
+describe('Teste de unidade do productsController', function () {
+
+  it('Listando os produtos', async function () {
+    const res = {};
+    const req = {};
+    const products = [productsList];
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon
+      .stub(productsService, 'findAll')
+      .resolves({ type: null, message: products });
+
+    await productsController.findAll(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(products);
+  });
+
+  it('Listando produto por id', async function () {
+    const res = {};
+    const req = { params: { id: 1 }};
+    const products = [productsList];
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon
+      .stub(productsService, 'findById')
+      .resolves({ type: null, message: products });
+
+    await productsController.findById(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(products);
+  });
+  
+  it('Listando produto inexistente', async function () {
+    const res = {};
+    const req = { params: { id: 5 }};
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon
+      .stub(productsService, 'findById')
+      .resolves({ type: 'Product id error', message: 'Product not found'});
+
+    await productsController.findById(req, res);
+
+    expect(res.status).to.have.been.calledWith(404);
+    expect(res.json).to.have.been.calledWith({ message: 'Product not found'});
+  });
+  
+  afterEach(sinon.restore);
+});
