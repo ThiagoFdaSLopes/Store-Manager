f5c0f538ecb6c1a17b0565bf907105fe397d34d1
Thiago <programadorthiagolopes@gmail.com>
2023-01-14 17:54:06 -0300

    feat: 99% de testes concluido

--- tests/unit/controllers/mock/salesProductsController.mock.js
+++ tests/unit/controllers/mock/salesProductsController.mock.js
@@ -11,9 +11,45 @@ const incorrectSaleQuantity = { produtcId: 1 };
 const incorrectSaleQuantityZero = { product: 1, quantity: 0 };
 const incorrectSaleQuantityNegative = { product: 1, quantity: -1 };
 
+const getAllSales = [
+  {
+    date: "2023-01-14T20:32:35.000Z",
+    saleId: 1,
+    productId: 1,
+    quantity: 5
+  },
+  {
+    date: "2023-01-14T20:32:35.000Z",
+    saleId: 1,
+    productId: 2,
+    quantity: 10
+  },
+  {
+    date: "2023-01-14T20:32:35.000Z",
+    saleId: 2,
+    productId: 3,
+    quantity: 15
+  }
+]
+
+const getSalesId = [
+  {
+    productId: 1,
+    date: "2023-01-14T20:32:35.000Z",
+    quantity: 5
+  },
+  {
+    productId: 2,
+    date: "2023-01-14T20:32:35.000Z",
+    quantity: 10
+  }
+];
+
 module.exports = {
   returnSale,
   productList,
+  getAllSales,
+  getSalesId,
   incorrectSaleId,
   incorrectSaleQuantity,
   incorrectSaleQuantityNegative,
--- tests/unit/controllers/salesProductsController.test.js
+++ tests/unit/controllers/salesProductsController.test.js
@@ -8,7 +8,7 @@ chai.use(sinonChai);
 const { salesProductsController } = require('../../../src/controllers');
 const { salesProductsService } = require('../../../src/services');
 
-const { productsList, incorrectSaleId } = require('./mock/salesProductsController.mock')
+const { productsList, incorrectSaleId, getAllSales, getSalesId } = require('./mock/salesProductsController.mock')
 
 describe('Teste de unidade do productsController', function () {
 
@@ -44,6 +44,57 @@ describe('Teste de unidade do productsController', function () {
 
     expect(res.status).to.have.been.calledWith(404);
     expect(res.json).to.have.been.calledWith({ message: 'Product not found' });
+  });
+
+  it('Listando todos sales', async function () {
+    const res = {};
+    const req = {};
+    const products = getAllSales;
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon
+      .stub(salesProductsService, 'findAllSales')
+      .resolves({ type: null, message: products });
+
+    await salesProductsController.findAllSales(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(products);
+  });
+
+  it('Listando por id errado', async function () {
+    const res = {};
+    const req = { params: { id: 100 }};
+    const products = getSalesId;
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon
+      .stub(salesProductsService, 'findById')
+      .resolves({ type: 'NOT_FOUND', message: 'Sale not found' });
+
+    await salesProductsController.findById(req, res);
+
+    expect(res.status).to.have.been.calledWith(404);
+    expect(res.json).to.have.been.calledWith({  message: 'Sale not found' });
+  });
+
+  it('Listando todos sales', async function () {
+    const res = {};
+    const req = { params: { id: 1 }};
+    const products = getSalesId;
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns();
+    sinon
+      .stub(salesProductsService, 'findById')
+      .resolves({ type: null, message: products });
+
+    await salesProductsController.findById(req, res);
+
+    expect(res.status).to.have.been.calledWith(200);
+    expect(res.json).to.have.been.calledWith(products);
   });
 
   afterEach(sinon.restore);
