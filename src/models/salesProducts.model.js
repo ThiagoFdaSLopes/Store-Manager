45bdfb598f04f16dec4aeba1c7422a92ede426a8
Thiago <programadorthiagolopes@gmail.com>
2023-01-14 17:28:13 -0300

    feat: Feito rotas para filtar por id e todas

--- src/models/salesProducts.model.js
+++ src/models/salesProducts.model.js
@@ -20,6 +20,32 @@ const newSaleProduct = async (productList) => {
   return { id, itemsSold: sale };
 };
 
+const findAllSales = async () => {
+  const QUERY = `SELECT sales.date as date, spr.sale_id as saleId, spr.product_id as productId,
+  spr.quantity as quantity
+  FROM StoreManager.sales AS sales
+  INNER JOIN sales_products AS spr ON sales.id = spr.sale_id`;
+  const [allSales] = await connection.execute(QUERY);
+
+  return allSales;
+};
+
+const findById = async (id) => {
+  const QUERY = `SELECT sale_id as saleId, date as date, product_id as productId,
+  quantity FROM sales as sa
+  INNER JOIN sales_products as spr
+  ON spr.sale_id = sa.id
+  HAVING sale_id = ?`;
+  const [allSales] = await connection.execute(QUERY, [id]);
+
+  const salesById = allSales.map(({ productId, date, quantity }) =>
+    ({ productId, date, quantity }));
+
+  return salesById;
+};
+
 module.exports = {
   newSaleProduct,
+  findAllSales,
+  findById,
 };
