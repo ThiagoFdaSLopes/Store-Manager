013e9b883e18c0486ea75e0abccf690752721c9b
Thiago <programadorthiagolopes@gmail.com>
2023-01-14 11:55:44 -0300

    feat: Criado middleware  quantity e ProductId

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,5 +1,9 @@
 const validateName = require('./validateName');
+const validateProductsid = require('./validateProductsId');
+const validateQuantity = require('./validateQuantity');
 
 module.exports = {
   validateName,
+  validateProductsid,
+  validateQuantity,
 };
