e819f4aa98e746694aca44adc80c992a38090398
Thiago <programadorthiagolopes@gmail.com>
2023-01-14 10:56:00 -0300

    fix: Controller Sales

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,7 @@
 const productsController = require('./products.controller');
+const salesProducts = require('./salesProducts.controller');
 
 module.exports = {
   productsController,
+  salesProducts,
 };
