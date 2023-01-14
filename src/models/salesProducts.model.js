91beeb539f066a7f74f1c51eb670104a27e4eae5
Thiago <programadorthiagolopes@gmail.com>
2023-01-14 11:57:40 -0300

    feat: Corrigido connection.execute

--- src/models/salesProducts.model.js
+++ src/models/salesProducts.model.js
@@ -1,9 +1,9 @@
 const connection = require('./db/connection');
 
 const newSaleProduct = async (saleId, productList) => {
-  productList.map((product) => connection.execute(
+  productList.map((item) => connection.execute(
       'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?)',
-    [saleId, product.productId, product.quantity],
+    [saleId, item.productId, item.quantity],
   ));
 
   return { id: saleId, itemsSold: productList };
