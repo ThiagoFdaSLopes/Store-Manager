83c2437349d8b1c5a91549968af120a052e887ce
Thiago <programadorthiagolopes@gmail.com>
2023-01-14 19:24:22 -0300

    feat: criado rota para deletar

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -36,9 +36,20 @@ const updateProductName = async (req, res) => {
   return res.status(200).json(message);
 };
 
+const deleteProductFromId = async (req, res) => {
+  const { id } = req.params;
+
+  const { type, message } = await productsService.deleteProductFromId(id);
+
+  if (type) return res.status(404).json({ message });
+
+  return res.status(204).send();
+};
+
 module.exports = {
   findAll,
   findById,
   createProduct,
   updateProductName,
+  deleteProductFromId,
 };
