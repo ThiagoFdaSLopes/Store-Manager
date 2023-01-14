4664d1c6b5112b65d9a287b5d8dfce0a0e820987
Thiago <programadorthiagolopes@gmail.com>
2023-01-14 13:16:34 -0300

    fix: Corrigindo query sales

--- src/models/sales.model.js
+++ src/models/sales.model.js
@@ -1,7 +1,7 @@
 const connection = require('./db/connection');
 
 const newSale = async () => {
-  const QUERY = 'INSERT INTO StoreManager.sales () VALUES ()';
+  const QUERY = 'INSERT INTO StoreManager.sales (date) VALUES (NOW())';
   const [{ insertId }] = await connection.execute(QUERY);
 
   return insertId;
