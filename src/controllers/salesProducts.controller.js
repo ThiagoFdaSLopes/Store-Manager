8a3a370dff42ffdac840e157325a112cbc589da0
Thiago <programadorthiagolopes@gmail.com>
2023-01-14 17:29:05 -0300

    feat: Rotas criadas

--- src/controllers/salesProducts.controller.js
+++ src/controllers/salesProducts.controller.js
@@ -8,6 +8,23 @@ const createNewSales = async (req, res) => {
   return res.status(201).json(message);
 };
 
+const findAllSales = async (_req, res) => {
+  const { message } = await salesProductsService.findAllSales();
+
+  return res.status(200).json(message);
+};
+
+const findById = async (req, res) => {
+  const { id } = req.params;
+  const { type, message } = await salesProductsService.findById(id);
+
+  if (type) return res.status(404).json({ message });
+
+  return res.status(200).json(message);
+};
+
 module.exports = {
   createNewSales,
+  findAllSales,
+  findById,
 };
