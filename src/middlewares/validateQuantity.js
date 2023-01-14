4286dc5845db97afa8342e3586323bc2d6c54bbe
Thiago <programadorthiagolopes@gmail.com>
2023-01-14 13:14:55 -0300

    fix: Validacao

--- src/middlewares/validateQuantity.js
+++ src/middlewares/validateQuantity.js
@@ -1,9 +1,15 @@
 const validateQuantity = (req, res, next) => {
   const bodyProducts = req.body;
-
-  const check = (array) => array.every((item) => item.quantity);
-
-  if (!check(bodyProducts)) return res.status(400).json({ message: '"quantity" is required' });
+  
+  const checkedQuantity = (array) => array.every((item) => !item.quantity);
+  const checkedQuantityValue = (array) =>
+    array.every((item) => item.quantity === 0 || item.quantity < 0);
+  if (checkedQuantityValue(bodyProducts)) {
+    return res.status(422).json({ message: '"quantity" must be greater than or equal to 1' });
+  }
+  if (checkedQuantity(bodyProducts)) {
+    return res.status(400).json({ message: '"quantity" is required' });
+  }
 
   next();
 };
