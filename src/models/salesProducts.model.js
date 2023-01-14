b08f0ed8bbe39f66981e9d3c40af0bcfbac5c652
Thiago <programadorthiagolopes@gmail.com>
2023-01-14 14:11:28 -0300

    feat: Alterado Criacao de id

--- src/models/salesProducts.model.js
+++ src/models/salesProducts.model.js
@@ -1,12 +1,18 @@
 const connection = require('./db/connection');
 
 const newSaleProduct = async (saleId, productList) => {
-  productList.map((item) => connection.execute(
-      'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?)',
-    [saleId, item.productId, item.quantity],
-  ));
+  const [[{ count }]] = await connection.execute(
+    'SELECT COUNT(distinct sale_id) as count FROM StoreManager.sales_products',
+  );
 
-  return { id: saleId, itemsSold: productList };
+  const id = count + 1;
+
+  productList.map((item) => connection.execute(
+    'INSERT INTO StoreManager.sales_products (sale_id, product_id, quantity) VALUES (?, ?, ?)',
+    [id, item.productId, item.quantity],
+    ));
+
+  return { id, itemsSold: productList };
 };
 
 module.exports = {
--- src/services/salesProducts.service.js
+++ src/services/salesProducts.service.js
@@ -1,11 +1,28 @@
-const { salesProducts, sales } = require('../models');
+const { salesProducts, sales, productsModel } = require('../models');
+
+const getAllIdsExist = async (productList) => {
+  const existInDb = await Promise.all(
+    productList.map(async (item) => {
+      const product = await productsModel.findById(item.productId);
+      if (product.length === 0) return false;
+      return true;
+    }),
+  );
+
+  return existInDb;
+};
 
 const createNewProductsSale = async (productList) => {
   const saleId = await sales.newSale();
 
-  const newSalesProducts = await salesProducts.newSaleProduct(saleId, productList);
+  const exist = await getAllIdsExist(productList);
+  const go = exist.every((item) => item === true);
 
-  if (newSalesProducts) return { type: null, message: newSalesProducts };
+  if (go) {
+    const newSalesProducts = await salesProducts.newSaleProduct(saleId, productList);
+    if (newSalesProducts) return { type: null, message: newSalesProducts };
+  }
+  return { type: 'NOT_FOUND', message: 'Product not found' };
 };
 
 module.exports = {
